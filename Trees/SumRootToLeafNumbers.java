c20e6486d9392157bfd95201a97c5544b1097edc
jennypeng <pengjenny@berkeley.edu>
2016-05-05 22:19:01 -0700

    more tree problem

---
+++ Trees/SumRootToLeafNumbers.java
@@ -0,0 +1,43 @@
+/* Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+For example,
+
+    1
+   / \
+  2   3
+     2
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+
+Return the sum = 12 + 13 = 25. */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private int totalSum = 0;
+    public int sumNumbers(TreeNode root) {
+      if (root == null) return totalSum;
+      findPathSum(root, 0);
+      return totalSum;
+    }
+    public void findPathSum(TreeNode root, int currPathSum) {
+      if (root == null) return;
+      if (root.left == null && root.right == null) {
+        totalSum += 10 * currPathSum + root.val;
+      } else {
+        findPathSum(root.left, currPathSum * 10 + root.val); // 1
+        findPathSum(root.right, currPathSum * 10 + root.val); // 1
+      }
+    }
+}
