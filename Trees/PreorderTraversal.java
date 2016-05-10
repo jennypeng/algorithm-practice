8b3f1ed3e6ef0de8057a28f9555b0c3e87cf5c00
jennypeng <pengjenny@berkeley.edu>
2016-05-09 22:51:25 -0700

    preorder

---
+++ Trees/PreorderTraversal.java
@@ -0,0 +1,15 @@
+/* Given a binary tree, return the preorder traversal of its nodes' values. */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+    }
+}
